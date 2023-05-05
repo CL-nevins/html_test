34269e3739f9f25afd82fadfb19b322923ce127f
nevins <nevins35@163.com>
2023-05-05 16:13:43 +0800

    原生骨架屏

---
+++ main.js
@@ -0,0 +1,12 @@
+// Get the wrapper element
+const wrapper = document.querySelector('.skeleton-wrapper');
+
+// Set the number of skeletons to create
+const skeletonCount = 5;
+
+// Create the skeletons and append them to the wrapper
+// for (let i = 0; i < skeletonCount; i++) {
+//   const skeleton = document.createElement('div');
+//   skeleton.classList.add('skeleton');
+//   wrapper.appendChild(skeleton);
+// }
