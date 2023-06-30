2ac77ba9230eb19ee49c6fdd9c223dc2def5213c
nevins <nevins35@163.com>
2023-06-30 17:06:04 +0800

    JavaScript 练习题

--- main.js
+++ main.js
@@ -1,15 +1,1 @@
-// parent 监听 iframe child 发送的消息
-window.addEventListener('message', (event) => {
-  if (event.origin !== 'http://localhost:9191') return;
-  console.log('parent recieved:', event.data);
-});
 
-// parent 给 iframe child 发消息
-const textMessageInput = document.querySelector('#textMessage');
-const myIframe = document.querySelector('#myIframe');
-textMessageInput.addEventListener('input', (event) => {
-  myIframe.contentWindow.postMessage(
-    event.currentTarget.value,
-    'http://localhost:9191'
-  );
-});
